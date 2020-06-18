eced36ad74df0e3b2b5e3104c5019080ba6e0c92
Hadi Khazaee Asl <Hadi_Khazaee_Asl@yahoo.com>
2020-06-19 00:59:48 +0430

    complete list page ...

--- src/app/pages/components/list/list.page.ts
+++ src/app/pages/components/list/list.page.ts
@@ -1,8 +1,13 @@
+import {
+  XColor,
+  XResourceIDs,
+  isNullOrEmptyString,
+  XBaseRangeRequestDto,
+} from 'x-framework-core';
 import { Component } from '@angular/core';
-import { XIconNames } from 'x-framework-components';
+import { XIconNames, XListItem } from 'x-framework-components';
 import { VPageComponent } from '../../../views/v-page/v-page.component';
 import { AppResourceIDs } from 'src/app/config/app.localization.config';
-import { XResourceIDs, isNullOrEmptyString, XColor } from 'x-framework-core';
 
 @Component({
   selector: 'app-list',
@@ -36,15 +41,200 @@ export class ListPage extends VPageComponent {
   //
   // Content ...
   readonly contentFa = `
-    # ${this.toolbarTitle}
-    `;
+  # ${this.toolbarTitle}
+  این مولفه جهت نمایش فهرستی از موارد و فراهم کردن ابزار مدیریت آن بکار می رود.
+  `;
 
   readonly contentEn = `
-    # ${this.toolbarTitle}
-    `;
+  # ${this.toolbarTitle}
+  this component used to provide a way to show items in a list and manage items.
+  `;
 
   //
-  readonly sample1 = '```' + '```';
+  readonly sample1 =
+    '```' +
+    `
+  tempList = Array.from(Array(25).keys());
+  templListItems: XListItem<string>[] = this.tempList.map((i) => {
+    const li: XListItem<string> = {
+      data: \`Item \${i.toString()}\`,
+      slideOptions: [
+        {
+          id: 'remove',
+          icon: this.IconNames.remove,
+          color: this.ColorNames.Danger,
+          onlyIcon: true,
+          slot: 'end',
+          handler: () => {
+            //
+            this.managerService.notificationService
+              .presentDangerNotification({
+              message: \`remove: \${i}\`,
+              dissmissable: true,
+            });
+          },
+        },
+        {
+          id: 'edit',
+          icon: this.IconNames.edit,
+          color: this.ColorNames.Warning,
+          onlyIcon: true,
+          slot: 'start',
+          handler: () => {
+            //
+            this.managerService.notificationService
+              .presentInfoNotification({
+              message: \`edit: \${i}\`,
+              dissmissable: true,
+            });
+          },
+        },
+      ],
+    };
+
+    return li;
+  });
+  ` +
+    '```';
+
+  //
+  readonly sample2 =
+    '```' +
+    `
+    <x-actions
+      [hasDelete]="true"
+      [hasSelectAll]="true"
+      [showSearchBar]="true"
+      [showActionBar]="true"
+      [hasDeselectAll]="true"
+      [hasInverseSelection]="true"
+      [listComponent]="listPresenter"
+      [actionBarColor]="ColorNames.Dark"
+      [searchBarColor]="ColorNames.Dark"
+      (removeItems)="handleRemoveItems(\$event)"
+      (queryChange)="listPresenter.filterItems(\$event)"
+    >
+    </x-actions>
+  ` +
+    `
+    <x-list
+      #listPresenter
+      [showDivider]="false"
+      [clickableItems]="true"
+      [selectableItems]="true"
+      [items]="templListItems"
+      [enableReordering]="true"
+      [itemTemplate]="listItemRef"
+      [color]="ColorNames.Tertiary"
+      (refresh)="handleRefreshList()"
+    >
+    </x-list>
+  ` +
+    '```';
+
+  readonly sample3 =
+    '```typescript' +
+    `
+  handleRefreshList() {
+    this.prepareListItems();
+  }
+  ` +
+    `
+  handleRemoveItems(request: XBaseRangeRequestDto<string>) {
+    //
+    this.templListItems = this.templListItems.filter(
+      (li) => !request.items.includes(li.data)
+    );
+  }
+  ` +
+    `
+  private prepareListItems() {
+    this.templListItems = this.tempList.map((i) => {
+      //
+      const li: XListItem<string> = {
+        data: \`Item $\{i.toString()}\`,
+        slideOptions: [
+          {
+            id: 'remove',
+            icon: this.IconNames.remove,
+            color: this.ColorNames.Danger,
+            onlyIcon: true,
+            slot: 'end',
+            handler: () => {
+              //
+              this.managerService.notificationService
+                .presentDangerNotification({
+                message: \`remove: \${i}\`,
+                dissmissable: true,
+              });
+            },
+          },
+          {
+            id: 'edit',
+            icon: this.IconNames.edit,
+            color: this.ColorNames.Warning,
+            onlyIcon: true,
+            slot: 'start',
+            handler: () => {
+              //
+              this.managerService.notificationService
+                .presentInfoNotification({
+                message: \`edit: \${i}\`,
+                dissmissable: true,
+              });
+            },
+          },
+        ],
+      };
+
+      //
+      return li;
+    });
+  }
+  ` +
+    '```';
+
+  //
+  tempList = Array.from(Array(25).keys());
+  templListItems: XListItem<string>[] = this.tempList.map((i) => {
+    //
+    const li: XListItem<string> = {
+      data: `Item ${i.toString()}`,
+      slideOptions: [
+        {
+          id: 'remove',
+          icon: this.IconNames.remove,
+          color: this.ColorNames.Danger,
+          onlyIcon: true,
+          slot: 'end',
+          handler: () => {
+            //
+            this.managerService.notificationService.presentDangerNotification({
+              message: `remove: ${i}`,
+              dissmissable: true,
+            });
+          },
+        },
+        {
+          id: 'edit',
+          icon: this.IconNames.edit,
+          color: this.ColorNames.Warning,
+          onlyIcon: true,
+          slot: 'start',
+          handler: () => {
+            //
+            this.managerService.notificationService.presentInfoNotification({
+              message: `edit: ${i}`,
+              dissmissable: true,
+            });
+          },
+        },
+      ],
+    };
+
+    //
+    return li;
+  });
 
   //
   //#region UI Providers ...
@@ -72,6 +262,70 @@ export class ListPage extends VPageComponent {
 
     //
     return result || '';
+  }
+  //#endregion
+
+  //
+  //#region UI Handlers ...
+  //
+  handleRefreshList() {
+    this.prepareListItems();
+  }
+
+  //
+  handleRemoveItems(request: XBaseRangeRequestDto<string>) {
+    //
+    this.templListItems = this.templListItems.filter(
+      (li) => !request.items.includes(li.data)
+    );
+  }
+  //#endregion
+
+  //
+  //#region Private ...
+  //
+  private prepareListItems() {
+    this.templListItems = this.tempList.map((i) => {
+      //
+      const li: XListItem<string> = {
+        data: `Item ${i.toString()}`,
+        slideOptions: [
+          {
+            id: 'remove',
+            icon: this.IconNames.remove,
+            color: this.ColorNames.Danger,
+            onlyIcon: true,
+            slot: 'end',
+            handler: () => {
+              //
+              this.managerService.notificationService.presentDangerNotification(
+                {
+                  message: `remove: ${i}`,
+                  dissmissable: true,
+                }
+              );
+            },
+          },
+          {
+            id: 'edit',
+            icon: this.IconNames.edit,
+            color: this.ColorNames.Warning,
+            onlyIcon: true,
+            slot: 'start',
+            handler: () => {
+              //
+              this.managerService.notificationService.presentInfoNotification({
+                message: `edit: ${i}`,
+                dissmissable: true,
+              });
+            },
+          },
+        ],
+      };
+
+      //
+      return li;
+    });
   }
   //#endregion
 }
