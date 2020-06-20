adb9d7dc0d719919aa2455de0864a8e78740dbe6
Hadi Khazaee Asl <Hadi_Khazaee_Asl@yahoo.com>
2020-06-21 04:28:04 +0430

    test x-action bar on tools page ...

--- src/app/pages/home/tools/tools.page.ts
+++ src/app/pages/home/tools/tools.page.ts
@@ -1,4 +1,12 @@
-import { Component, Input } from '@angular/core';
+import {
+  XActionBarItem,
+  defaultActionBarRemoveItem,
+  defaultActionBarSelectAllItem,
+  defaultActionBarDeSelectAllItem,
+  defaultActionBarInverseSelectionItem,
+} from 'x-framework-components';
+import { XColor } from 'x-framework-core';
+import { Component } from '@angular/core';
 import { VPageComponent } from '../../../views/v-page/v-page.component';
 import { AppResourceIDs } from 'src/app/config/app.localization.config';
 
@@ -18,5 +26,47 @@ export class ToolsPage extends VPageComponent {
   toolbarSubTitle = this.resourceProvider(AppResourceIDs.tools_description);
   toolbarShowSubTitle = true;
   //#endregion
+
+  actionBarItems: XActionBarItem[] = [
+    //
+    // Remove ...
+    {
+      //
+      ...defaultActionBarRemoveItem,
+
+      //
+      index: 0,
+
+      //
+      color: XColor.Light,
+    },
+    //
+    // Select All ...
+    {
+      //
+      ...defaultActionBarSelectAllItem,
+
+      //
+      index: 1,
+    },
+    //
+    // Inverse Selection ...
+    {
+      //
+      ...defaultActionBarInverseSelectionItem,
+
+      //
+      index: 2,
+    },
+    //
+    // De Select All ...
+    {
+      //
+      ...defaultActionBarDeSelectAllItem,
+
+      //
+      index: 3,
+    },
+  ];
   //#endregion
 }
