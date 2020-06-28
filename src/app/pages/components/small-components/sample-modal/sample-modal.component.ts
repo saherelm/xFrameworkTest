9b0636dea2f4eb404ef978a9132959d16c8d57dd
saherelm <Hadi_Khazaee_Asl@yahoo.com>
2020-06-28 14:40:46 +0430

    last works ...

--- src/app/pages/components/small-components/sample-modal/sample-modal.component.ts
+++ src/app/pages/components/small-components/sample-modal/sample-modal.component.ts
@@ -1,11 +1,16 @@
 import { Component, Input } from '@angular/core';
+import { XBaseComponent } from 'x-framework-components';
 
 @Component({
   selector: 'app-sample-modal',
   templateUrl: './sample-modal.component.html',
   styleUrls: ['./sample-modal.component.scss'],
 })
-export class SampleModalComponent {
+export class SampleModalComponent extends XBaseComponent {
   @Input()
   context: string;
+
+  handleClosePopup() {
+    this.managerService.dialogService.popoverController.dismiss();
+  }
 }
