584bb718d58cf03f06654ddb3c97ffb11781f7f3
Hadi Khazaee Asl <Hadi_Khazaee_Asl@yahoo.com>
2020-07-04 05:08:05 +0430

    last works on form ...

--- src/app/pages/components/form/form.page.ts
+++ src/app/pages/components/form/form.page.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
 import {
+  XColor,
+  XResourceIDs,
+  isNullOrEmptyString,
+  XColorWithBrightness,
+  toNormalString,
+} from 'x-framework-core';
+import { Subject, BehaviorSubject } from 'rxjs';
+import {
+  XListItem,
   XIconNames,
   XFormConfig,
   XFormUpdateOn,
   XFormControlType,
+  XFormControlValueChangeEventModel,
+  XFormControlStatusChangeEventModel,
 } from 'x-framework-components';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { VPageComponent } from '../../../views/v-page/v-page.component';
 import { AppResourceIDs } from 'src/app/config/app.localization.config';
-import { XResourceIDs, isNullOrEmptyString, XColor } from 'x-framework-core';
 
 interface XFormModel {
   firstName: string;
@@ -63,49 +73,145 @@ export class FormPage extends VPageComponent {
   readonly sample1 = '```' + '```';
 
   //
-  xFormConfig: XFormConfig<XFormModel> = {
-    name: 'PersonalForm',
-    model: {
-      firstName: 'Hadi',
-      lastName: 'Khazaee Asl',
-    },
-    updateOn: XFormUpdateOn.CHANGE,
-    controls: [
-      {
-        index: 0,
-        propName: 'firstName',
-        type: {
-          type: XFormControlType.Text,
-        },
-        appearance: {
-          label: 'First Name',
-          icons: {
-            suffix: {
-              name: this.IconNames.first_name,
-              applyStateColor: true,
-            },
-          },
-        },
-      },
-      {
-        index: 1,
-        propName: 'lastName',
-        type: {
-          type: XFormControlType.Text,
-        },
-        appearance: {
-          label: 'Last Name',
-          tooltip: 'Insert your Last Name here ...',
-          icons: {
-            suffix: {
-              name: this.IconNames.first_name,
-              applyStateColor: true,
-            },
-          },
-        },
-      },
-    ],
-  };
+  private NAME_AUTO_CONMPLETE_REF: TemplateRef<any>;
+
+  @ViewChild('nameAutoCompleteRef')
+  set nameAutoCompleteRef(v: TemplateRef<any>) {
+    //
+    if (!v) {
+      return;
+    }
+
+    //
+    this.NAME_AUTO_CONMPLETE_REF = v;
+    this.prepareFormConfig();
+    this.detectChanges();
+  }
+
+  get nameAutoCompleteRef() {
+    return this.NAME_AUTO_CONMPLETE_REF;
+  }
+
+  //
+  private nameAutoCompleteQuery = '';
+  private autoCompleteItems: XListItem<string>[] = ['HADI'].map((i) => {
+    return {
+      data: i,
+    } as XListItem<string>;
+  });
+
+  //
+  nameStrings: string[] = [
+    'Hadi',
+    'Hamid',
+    'Hesam',
+    'Mehdi',
+    'Kourosh',
+    'Daruosh',
+    'Raymond Reed',
+    'Willie Ward',
+    'Stephen Edwards',
+    'Albert Johnson',
+    'Scott Long',
+    'Samuel Bailey',
+    'Louis Ramirez',
+    'Philip Henderson',
+    'Jacqueline Bennett',
+    'Sharon Hernandez',
+    'Karen Morris',
+    'Theresa Parker',
+    'Judith Coleman',
+    'Martha White',
+    'Jose Mitchell',
+    'Carl Powell',
+    'Sandra Lopez',
+    'Douglas Wright',
+    'Helen Young',
+    'Kathryn Hughes',
+    'Kathy Bell',
+    'Jonathan Price',
+    'Paul Morgan',
+    'Deborah Roberts',
+    'Teresa Martinez',
+    'Brenda Williams',
+    'Todd Phillips',
+    'Clarence Collins',
+    'Alan Butler',
+    'Donna Torres',
+    'Mildred Perry',
+    'Katherine Walker',
+    'William Flores',
+    'Marilyn Brooks',
+    'Timothy Clark',
+    'Ronald Cook',
+    'Frank Hall',
+    'Pamela Rogers',
+    'Harold Sanders',
+    'Harry Hill',
+    'Evelyn Thompson',
+    'Charles Jenkins',
+    'Andrew Murphy',
+    'Juan Simmons',
+    'Patricia Diaz',
+    'Doris Smith',
+    'Janice Turner',
+    'Matthew Rivera',
+    'Joyce Adams',
+    'Joe Moore',
+    'Nancy Kelly',
+    'Peter Garcia',
+    'Virginia Ross',
+    'Rose James',
+    'Donald Nelson',
+    'Joan Lewis',
+    'Julia Perez',
+    'Gerald Washington',
+    'Irene Allen',
+    'Brandon Green',
+    'Melissa Cooper',
+    'Sara Taylor',
+    'Kelly Martin',
+    'Daniel Brown',
+    'Robert Evans',
+    'Susan Carter',
+    'James Thomas',
+    'Jack Bryant',
+    'Eugene Sanchez',
+    'Shirley Davis',
+    'Carol Wilson',
+    'Ryan Robinson',
+    'Roy Griffin',
+    'Phillip Alexander',
+    'Thomas Miller',
+    'Keith Russell',
+    'Frances Rodriguez',
+    'Diana Wood',
+    'Brian Jones',
+    'Gregory Jackson',
+    'Kenneth Patterson',
+    'Anne Stewart',
+    'Justin King',
+    'Benjamin Scott',
+    'Judy Watson',
+    'Ruth Gonzalez',
+    'Walter Anderson',
+    'Steven Peterson',
+    'Heather Gonzales',
+    'Billy Lee',
+    'Betty Campbell',
+    'Carolyn Richardson',
+    'Jesse Gray',
+    'Kevin Cox',
+    'Denise Foster',
+    'Fred Barnes',
+    'Nicholas Howard',
+    'Christina Baker',
+    'Laura Harris',
+    'Jennifer',
+  ];
+
+  //
+  xFormConfig: XFormConfig<XFormModel>;
 
   //
   //#region UI Providers ...
@@ -133,6 +239,118 @@ export class FormPage extends VPageComponent {
 
     //
     return result || '';
+  }
+  //#endregion
+
+  //
+  //#region Private ...
+  private handleAutoCompleteNames() {
+    // //
+    // const q = toNormalString(v);
+    // if (q === nameAutoCompleteQuery) {
+    //   return [];
+    // }
+    // //
+    // nameAutoCompleteQuery = q;
+    // console.log('qqqq: ', q);
+    // //
+    // const filteredItems = this.nameStrings.filter(
+    //   (n) => toNormalString(n).includes(q) && toNormalString(n) !== q
+    // );
+    // console.log('filteredItems: ', filteredItems);
+    // if (this.hasChild(filteredItems)) {
+    //   return filteredItems;
+    // }
+    // //
+    // return [];
+  }
+
+  private prepareFormConfig() {
+    //
+    this.xFormConfig = {
+      name: 'PersonalForm',
+      model: {
+        firstName: 'Hadi',
+        lastName: 'Khazaee Asl',
+      },
+      updateOn: XFormUpdateOn.CHANGE,
+      controls: [
+        {
+          index: 0,
+          propName: 'firstName',
+          type: {
+            type: XFormControlType.Text,
+          },
+          appearance: {
+            label: 'First Name',
+            icons: {
+              suffix: {
+                name: this.IconNames.first_name,
+                applyStateColor: true,
+              },
+            },
+            //
+            // TODO: Fix Form auto Complete ...
+            autoComplete: {
+              itemTemplate: this.NAME_AUTO_CONMPLETE_REF,
+              color: XColorWithBrightness.Tertiary,
+              items: () => {
+                return this.autoCompleteItems;
+              },
+              //   onSelect: (item: string) => {
+              //     console.log('auto complete selected: ', item);
+              //   },
+            },
+          },
+          eventHandlers: {
+            onBlured: (name: any) => {
+              console.log('control: ', name, ', was blured ...');
+            },
+            onFocused: (name: any) => {
+              console.log('control: ', name, ', was focused ...');
+            },
+            statusChanged: (model: XFormControlStatusChangeEventModel) => {
+              console.log('status changed: ', model);
+            },
+            valueChanged: (model: XFormControlValueChangeEventModel) => {
+              console.log('value changed: ', model);
+            },
+          },
+        },
+        {
+          index: 1,
+          propName: 'lastName',
+          type: {
+            type: XFormControlType.Text,
+          },
+          appearance: {
+            label: 'Last Name',
+            tooltip: 'Insert your Last Name here ...',
+            icons: {
+              prefix: {
+                applyStateColor: true,
+                name: this.IconNames.first_name,
+                color: XColorWithBrightness.Dark,
+              },
+            },
+          },
+          eventHandlers: {
+            onBlured: (name: any) => {
+              console.log('control: ', name, ', was blured ...');
+            },
+            onFocused: (name: any) => {
+              console.log('control: ', name, ', was focused ...');
+            },
+            statusChanged: (model: XFormControlStatusChangeEventModel) => {
+              console.log('status changed: ', model);
+            },
+            valueChanged: (model: XFormControlValueChangeEventModel) => {
+              console.log('value changed: ', model);
+            },
+          },
+        },
+      ],
+    };
   }
   //#endregion
 }
